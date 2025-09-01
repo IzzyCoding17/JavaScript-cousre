419ecde227cafe0791e5d622c3bd36265eda2f1d
IzzyCoding17 <izzysenpeak17@gmal.com>
2025-09-01 20:22:19 +0100

    updated

--- scripts/12-ass.js
+++ scripts/12-ass.js
@@ -20,15 +20,18 @@ runTwice(add); */
   // Grouping the JavaScript code into a function
   // is optional, but it is a bit cleaner.
 
-  function updateBtn() {
+  document.querySelector('.js-btn').addEventListener('click', ()=> {
     const button = document.querySelector('.js-btn');
+    
 
 
     button.innerHTML = 'Loading...'
-    setTimeout(function() {
+    setTimeout(() =>{
       button.innerHTML = 'Finished';
     }, 1000);
-  }
+  })
+    
+  
 
   //12e/f
   let timeoutId;
@@ -49,7 +52,7 @@ runTwice(add); */
 
   //12g/h/i
   let mssg = 2;
-setInterval(function() {
+setInterval(() =>{
   if(document.title === 'App') {
     document.title = `(${mssg})New messages`;
   }else{
@@ -104,4 +107,77 @@ setInterval(function() {
         clearInterval(intervalId);
         document.title = 'App';
       } */
+
+
+ //12j/k
+ const multiply = (a, b) => {
+return a * b;
+ };
+
+ //const multiply = (a, b) => a*b; //shorter way to write the code
+
+ console.log(multiply(2, 3));
+ console.log(multiply(7, 10));
+
+ //12l
+
+ function countPositive(nums){
+  let positiveNumbers = 0;
+
+    // When there is only 1 parameter,
+    // the brackets are optional. So
+    // we can also write: num => { ... }
+
+    nums.forEach((num) => {
+        if (num > 0){
+          positiveNumbers++;
+        }
+    });
+    return positiveNumbers;
+ }
+
+ console.log(countPositive([1, -3, 5]));
+ console.log(countPositive([-2, 3, -5, 7, 10]));
+
+ //12m
+ function addNum(array, num){
+    // When there is only 1 parameter,
+    // the brackets are optional. So we
+    // can also write: value => value + num
  
+    return array.map((value) => value + num);
+ }
+
+ console.log(addNum([1, 2, 3], 2));
+ console.log(addNum([-2, -1, 0, 99], 2))
+
+ //12n
+
+ function removeEgg(foods){
+    // If food is 'egg', the inner function returns false
+    //   and the food is not included in the result.
+    // Otherwise, the inner function returns true
+    //   and the food is included in the result. 
+
+    return foods.filter((food) => food !== 'egg');
+ }
+
+    console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
+
+   //12o
+     function removeEggs(foods){
+     let eggsRemoved = 0;
+
+    return foods.filter((food) =>{
+      // If the food is 'egg', we should return false
+      // but only if we haven't removed 2 eggs already.
+      if(food === 'egg' && eggsRemoved < 2){
+        eggsRemoved++;
+        return false;
+      }
+      return true;
+                   
+    });
+ }
+
+    console.log(removeEggs(['egg', 'apple', 'egg', 'egg', 'ham']));

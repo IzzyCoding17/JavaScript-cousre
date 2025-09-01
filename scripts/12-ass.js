9dedbe3f8447917ba89337a54fcf3d47e6d6ab40
IzzyCoding17 <izzysenpeak17@gmal.com>
2025-09-01 14:44:34 +0100

    updated

--- scripts/12-ass.js
+++ scripts/12-ass.js
@@ -30,9 +30,9 @@ runTwice(add); */
     }, 1000);
   }
 
-  //12e/h
+  //12e/f
   let timeoutId;
-   
+
   function displayMssg() {
     const messgElement = document.querySelector('.js-add-btn');
     messgElement.innerHTML = 'Added'
@@ -47,3 +47,61 @@ runTwice(add); */
     }, 2000)
   }
 
+  //12g/h/i
+  let mssg = 2;
+setInterval(function() {
+  if(document.title === 'App') {
+    document.title = `(${mssg})New messages`;
+  }else{
+    document.title = 'App';
+  }
+
+  if (mssg === 0){
+    document.title = 'App';
+  }else if (mssg > 0){
+
+  }
+}, 1000);
+
+      //  Note: This is just a sample solution. Other
+      // solutions are possible so don't worry if
+      // your solution looks different.
+/*    let messages = 2;
+
+      // Save the intervalId in case we need to cancel it.
+      let intervalId;
+
+      // We'll use this variable to keep track of whether
+      // or not we're displaying the notification.
+      let isDisplayingNotification;
+
+      // Start displaying the notification in the tab
+      // when the page first loads.
+      displayNotification();
+
+      function displayNotification() {
+        // If we're already displaying the notification,
+        // stop this function because we don't want to
+        // create 2 intervals at the same time.
+        if (isDisplayingNotification) {
+          return;
+        }
+
+        isDisplayingNotification = true;
+
+        intervalId = setInterval(function() {
+          if (document.title === 'App') {
+            document.title = `(${messages}) New messages`;
+          } else {
+            document.title = 'App';
+          }
+        }, 1000);
+      }
+
+      function stopNotification() {
+        isDisplayingNotification = false;
+
+        clearInterval(intervalId);
+        document.title = 'App';
+      } */
+ 
